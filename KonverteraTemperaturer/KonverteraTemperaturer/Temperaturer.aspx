4069df051417f19b90e2f8af925570267a581f9e
nr222cb <nr222cb@student.lnu.se>
2014-02-02 18:24:31 +0100

    Validerinskontroller klara

--- KonverteraTemperaturer/KonverteraTemperaturer/Temperaturer.aspx
+++ KonverteraTemperaturer/KonverteraTemperaturer/Temperaturer.aspx
@@ -10,20 +10,41 @@
     <form id="form1" runat="server" defaultbutton="KonvButton">
     <div>
         <h1>Konvertera temperaturer</h1>
+        <%-- Felmeddelanden --%>
+            <asp:ValidationSummary ID="ValidationSummary1" runat="server" HeaderText="Fel inträffade. Korrigera och försök igen." ShowMessageBox="True" />
         <%-- Starttemperatur --%>
         <div>
             <asp:Label ID="Label1" runat="server" Text="Starttemperatur:" AssociatedControlID="StartTemp"></asp:Label>
             <asp:TextBox ID="StartTemp" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Fyll i en starttemperatur!" 
+                ControlToValidate="StartTemp" Display="Dynamic" Text="*" SetFocusOnError="True"></asp:RequiredFieldValidator>
+            <asp:CompareValidator ID="CompareValidator1" runat="server" ErrorMessage="Texten måste kunna tolkas som heltal." 
+                Display="Dynamic" ControlToValidate="StartTemp" SetFocusOnError="True" 
+                Operator="DataTypeCheck" Type="Integer" Text="*"></asp:CompareValidator>
         </div>
         <%-- Sluttemperatur --%>
         <div>
             <asp:Label ID="Label2" runat="server" Text="Sluttemperatur:" AssociatedControlID="SlutTemp"></asp:Label>
             <asp:TextBox ID="SlutTemp" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="Fyll i en sluttemperatur!" ControlToValidate="SlutTemp" Display="Dynamic" SetFocusOnError="True" Text="*"></asp:RequiredFieldValidator>
+            <asp:CompareValidator ID="CompareValidator2" runat="server" ErrorMessage="Texten måste kunna tolkas som heltal." 
+                Display="Dynamic" ControlToValidate="SlutTemp" SetFocusOnError="True" 
+                Operator="DataTypeCheck" Type="Integer" Text="*"></asp:CompareValidator>
+            <asp:CompareValidator ID="CompareValidator4" runat="server" ErrorMessage="Sluttemperaturen måste vara högre än starttemperaturen." 
+                Display="Dynamic" ControlToValidate="SlutTemp" SetFocusOnError="True" 
+                Operator="GreaterThan" Type="Integer" Text="*" ControlToCompare="StartTemp"></asp:CompareValidator>  
         </div>
         <%-- Temperatursteg --%>
         <div>
-            <asp:Label ID="Label3" runat="server" Text="Temperatursteg:" AssociatedControlID="SlutTemp"></asp:Label>
+            <asp:Label ID="Label3" runat="server" Text="Temperatursteg:" AssociatedControlID="TempSteg"></asp:Label>
             <asp:TextBox ID="TempSteg" runat="server"></asp:TextBox>
+            <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ErrorMessage="Fyll i ett temperatursteg!" ControlToValidate="TempSteg" Display="Dynamic" Text="*" SetFocusOnError="True"></asp:RequiredFieldValidator>
+            <asp:CompareValidator ID="CompareValidator3" runat="server" ErrorMessage="Texten måste kunna tolkas som heltal." 
+                Display="Dynamic" ControlToValidate="TempSteg" SetFocusOnError="True" 
+                Operator="DataTypeCheck" Type="Integer" Text="*"></asp:CompareValidator>
+            <asp:RangeValidator ID="RangeValidator1" runat="server" ErrorMessage="Temperatursteget måste ligga mellan 1 och 100." 
+                Display="Dynamic" Text="*" ControlToValidate="TempSteg" MaximumValue="100" MinimumValue="1" 
+                SetFocusOnError="True" Type="Integer"></asp:RangeValidator>       
         </div>
         <%-- Välj typ av konvertering --%>
         <div>
--- KonverteraTemperaturer/KonverteraTemperaturer/Temperaturer.aspx.designer.cs
+++ KonverteraTemperaturer/KonverteraTemperaturer/Temperaturer.aspx.designer.cs
@@ -22,6 +22,15 @@ namespace KonverteraTemperaturer {
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
         
         /// <summary>
+        /// ValidationSummary1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.ValidationSummary ValidationSummary1;
+        
+        /// <summary>
         /// Label1 control.
         /// </summary>
         /// <remarks>
@@ -38,6 +47,24 @@ namespace KonverteraTemperaturer {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox StartTemp;
+        
+        /// <summary>
+        /// RequiredFieldValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+        
+        /// <summary>
+        /// CompareValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator CompareValidator1;
         
         /// <summary>
         /// Label2 control.
@@ -58,6 +85,33 @@ namespace KonverteraTemperaturer {
         protected global::System.Web.UI.WebControls.TextBox SlutTemp;
         
         /// <summary>
+        /// RequiredFieldValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator2;
+        
+        /// <summary>
+        /// CompareValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator CompareValidator2;
+        
+        /// <summary>
+        /// CompareValidator4 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator CompareValidator4;
+        
+        /// <summary>
         /// Label3 control.
         /// </summary>
         /// <remarks>
@@ -74,6 +128,33 @@ namespace KonverteraTemperaturer {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox TempSteg;
+        
+        /// <summary>
+        /// RequiredFieldValidator3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator3;
+        
+        /// <summary>
+        /// CompareValidator3 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CompareValidator CompareValidator3;
+        
+        /// <summary>
+        /// RangeValidator1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RangeValidator RangeValidator1;
         
         /// <summary>
         /// Label4 control.
